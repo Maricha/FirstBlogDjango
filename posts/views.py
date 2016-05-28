4889276b3b87e442eee2627ee1f5990d042ed14b
Maricha <maricha177@gazeta.pl>
2016-05-28 23:20:00 +0200

    static, delete and urls update

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Post
+from .forms  import PostForm
 # Create your views here.
 
 def post_create(request):
-	return HttpResponse("hello")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save();
+		messages.success(request, "Successfully Created")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	else:
+		messages.error(request, "Not Sueccessfully Created")
+	context = {
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
 	instance = get_object_or_404(Post, id=id)
@@ -31,10 +44,25 @@ def post_list(request):
 		"title": "List",
 	}
 	
-	return render(request, "index.html", context)
+	return render(request, "post_list.html", context)
 
-def post_update(request):
-	return HttpResponse("hello")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save();
+		messages.success(request, "Successfully Save")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
 
-def post_delete(request):
-	return HttpResponse("hello")
+def post_delete(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()
+	messages.success(request, "Successfully Created")
+	return redirect("posts:list")
