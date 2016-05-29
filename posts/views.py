479d056b1d73200f979db7240ac72cf5c221a831
Maricha <maricha177@gazeta.pl>
2016-05-29 15:09:38 +0200

    permission user

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
+from urllib import quote_plus
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -9,6 +10,8 @@ from .forms  import PostForm
 # Create your views here.
 
 def post_create(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -22,9 +25,11 @@ def post_create(request):
 
 def post_detail(request, id=None):
 	instance = get_object_or_404(Post, id=id)
+	share_string = quote_plus(instance.content)
 	context = {
 		"title": instance.title,
 		"instance": instance,
+		"share_string": share_string,
 	}
 	return render(request, "post_detail.html", context)
 
@@ -59,6 +64,8 @@ def post_list(request):
 	return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, id=id)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
@@ -74,6 +81,8 @@ def post_update(request, id=None):
 	return render(request, "post_form.html", context)
 
 def post_delete(request, id=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, id=id)
 	instance.delete()
 	messages.success(request, "Successfully Created")
